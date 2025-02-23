d7e487e1797c89eca6f06c814d820589c9e5b67d
Eduardo Sanchez <eddiekidd98@gmail.com>
2025-02-23 12:09:39 -0600

    adding cors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-import authRoutes from './routes/auth.routes.js'
-import adminRoutes from './routes/admin.routes.js'
-import templateRoutes from './routes/template.routes.js'
-import completedTemplateRoutes from './routes/completedTemplate.routes.js'
+import authRoutes from './routes/auth.routes.js';
+import adminRoutes from './routes/admin.routes.js';
+import templateRoutes from './routes/template.routes.js';
+import completedTemplateRoutes from './routes/completedTemplate.routes.js';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+};
 
-app.use('/api', authRoutes)
-app.use('/api', adminRoutes)
-app.use('/api', templateRoutes)
-app.use('/api', completedTemplateRoutes)
+app.use(cors(corsOptions));
+app.use(express.json());
 
+app.use('/api', authRoutes);
+app.use('/api', adminRoutes);
+app.use('/api', templateRoutes);
+app.use('/api', completedTemplateRoutes);
 
-
-
-app.listen(3500)
-console.log('Server on port: ', 3500)
+const port = process.env.PORT || 3500;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
