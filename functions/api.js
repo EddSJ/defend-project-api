a78d1d87b18c5b1597462f685d54bb8d81ec51fe
Eduardo Sanchez <eddiekidd98@gmail.com>
2025-02-23 12:50:24 -0600

    fixes

--- functions/api.js
+++ functions/api.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const app = express();
+const cors = require('cors');
 
 const authRoutes = require('../src/routes/auth.routes');
 const adminRoutes = require('../src/routes/admin.routes');
 const templateRoutes = require('../src/routes/template.routes');
 const completedTemplateRoutes = require('../src/routes/completedTemplate.routes');
 
-app.use(express.json());
+const app = express();
 
-const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 app.use('/api', authRoutes);
 app.use('/api', adminRoutes);
